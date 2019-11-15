43c5981c4c00d6eb2c45905f97bf01aae6720243
Lucas Ouwens <lucasouwens@gmail.com>
2019-11-15 14:11:48 +0100

    Implemented the preparations for the usage of Caliburn Micro.

---
+++ EvernoteClone/EvernoteCloneGUI/ViewModels/NoteFeverViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteCloneGUI.ViewModels
+{
+    /// <summary>
+    /// The root viewmodel, this is the viewmodel which handles the main application screen.
+    /// </summary>
+    public class NoteFeverViewModel : Screen
+    {
+        
+    }
+}
