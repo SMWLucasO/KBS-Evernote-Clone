9f6515a3192c864234a3ff9bd6d2aedc83af8bc5
Lucas Ouwens <lucasouwens@gmail.com>
2019-11-17 19:37:48 +0100

    Implementation of classes for proper database access. Using repository pattern and models.

---
+++ EvernoteClone/EvernoteCloneLibrary/Database/DataAccess.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteCloneLibrary.Database
+{
+    /// <summary>
+    /// The purpose of this class is giving access to data, specifically to the database data.
+    /// </summary>
+    public class DataAccess
+    {
+
+        private SqlConnection _connection;
+
+        /// <summary>
+        /// A method for querying the database.
+        /// </summary>
+        /// <remarks>
+        /// The dictionary should have keys as placeholder name and values as the placeholder value of the query.
+        /// Two default ReturnType methods are provided.
+        /// See: <see cref="DataAccess.RowsAffectedReturnType">RowsAffectedReturnType</see> and <see cref="DataAccess.SqlDataReaderReturnType">SqlDataReaderReturnType</see> 
+        /// </remarks>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="QueryString"></param>
+        /// <param name="Parameters"></param>
+        /// <param name="ReturnType"></param>
+        /// <returns></returns>
+        public T Query<T>(string QueryString, Dictionary<string, object> Parameters, Func<SqlCommand, T> ReturnType)
+        {
+            SqlConnection sqlConnection = OpenSqlConnection();
+            using (SqlCommand sqlCommand = GenerateParameteredCommand(QueryString, sqlConnection, Parameters))
+            {
+                return ReturnType(sqlCommand);
+            }
+        }
+
+        /// <summary>
+        /// Method which prepares the SqlCommand for being executed
+        /// </summary>
+        /// <param name="QueryString">string</param>
+        /// <param name="Connection">SqlConnection</param>
+        /// <param name="Parameters"></param>
+        /// <returns>SqlCommand</returns>
+        private SqlCommand GenerateParameteredCommand(string QueryString, SqlConnection Connection, Dictionary<string, object> Parameters)
+        {
+            SqlCommand command = new SqlCommand(QueryString, Connection);
+            if (Parameters != null)
+            {
+                foreach (string key in Parameters.Keys)
+                {
+                    command.Parameters.AddWithValue(key, Parameters[key]);
+                }
+            }
+
+            return command;
+        }
+
+        /// <summary>
+        /// Method for creating a connection to the database.
+        /// </summary>
+        /// <returns>SqlConnection</returns>
+        private SqlConnection OpenSqlConnection()
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=.;Initial Catalog=NoteFever_EvernoteClone;Integrated Security=True");
+
+            sqlConnection.Open();
+
+            return sqlConnection;
+        }
+
+        /// <summary>
+        /// Since we don't close the connection in a method, we need to do it explicitly somewhere.
+        /// Therefore, this should be done after a query is executed (and perhaps read.)
+        /// </summary>
+        public void CloseSqlConnection()
+        {
+            _connection?.Close();
+        }
+
+        /// <summary>
+        /// A return type for the Query method, this is used for checking if insert/update/delete
+        /// queries were successful.
+        /// </summary>
+        public static Func<SqlCommand, bool> RowsAffectedReturnType = sqlCommand
+            => sqlCommand.ExecuteNonQuery() >= 1;
+
+        /// <summary>
+        /// A return type for the Query method, this is used for retrieving selected data.
+        /// </summary>
+        public static Func<SqlCommand, SqlDataReader> SqlDataReaderReturnType = sqlCommand
+            => sqlCommand.ExecuteReader();
+
+    }
+}
---
+++ EvernoteClone/EvernoteCloneLibrary/Database/IRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteCloneLibrary.Database
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> where T : IModel
+    {
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conditions"></param>
+        /// <param name="Parameters"></param>
+        /// <returns></returns>
+        IEnumerable<T> GetBy(string[] conditions, Dictionary<string, object> Parameters);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ToInsert"></param>
+        /// <returns></returns>
+        bool Insert(T ToInsert);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ToUpdate"></param>
+        /// <returns></returns>
+        bool Update(T ToUpdate);
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ToDelete"></param>
+        /// <returns></returns>
+        bool Delete(T ToDelete);
+
+    }
+}
