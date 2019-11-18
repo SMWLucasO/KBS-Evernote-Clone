cb821f1e77b0dbc8df3000ac4a5e08730f146d91
Lucas Ouwens <lucasouwens@gmail.com>
2019-11-18 18:16:48 +0100

    Implemented the local system saving (export, import) and the database saving is done too. The exporting method is kind of dirty and not entirely done due to the current priorities for this project. Please don't hurt me for the use of hard-coded XML nodes in representations.

---
+++ EvernoteClone/EvernoteCloneLibrary/Files/Parsers/IParseable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteCloneLibrary.Files.Parsers
+{
+    public interface IParseable
+    {
+        public string[] ToXMLRepresentation();
+    }
+}
---
+++ EvernoteClone/EvernoteCloneLibrary/Files/Parsers/XMLExporter.cs
@@ -0,0 +1,34 @@
+﻿using EvernoteCloneLibrary.Notebooks;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteCloneLibrary.Files.Parsers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class XMLExporter
+    {
+        // NOTE: This class can be further abstracted by changing Notebook notebook to IParseable item
+        // or something alike.
+
+        /// <summary>
+        /// Method to export notebooks to XML form.
+        /// </summary>
+        /// <param name="PathName"></param>
+        /// <param name="Filename"></param>
+        /// <param name="Notebook"></param>
+        /// <returns></returns>
+        public static bool Export(string PathName, string Filename, Notebook Notebook)
+        {
+            string fullPath = $"{PathName}/{Filename}";
+            File.WriteAllLines(fullPath, Notebook.ToXMLRepresentation());
+
+            return File.Exists(fullPath);
+        }
+    }
+}
