86ef0d13eac2f2696bbc66d5c70085144fceb03a
Lucas Ouwens <lucasouwens@gmail.com>
2019-11-23 16:45:20 +0100

    GUI should now properly pop-up the editor.

---
+++ EvernoteClone/EvernoteCloneGUI/ViewModels/NewNoteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteCloneGUI.ViewModels
+{
+    public class NewNoteViewModel : Screen 
+    {
+
+        // TODO add all bindings
+
+        public NewNoteViewModel()
+        {
+            DisplayName = "NoteFever | Nameless note";
+        }
+
+    }
+}
--- EvernoteClone/EvernoteCloneGUI/ViewModels/NoteFeverViewModel.cs
+++ EvernoteClone/EvernoteCloneGUI/ViewModels/NoteFeverViewModel.cs
@@ -1,17 +1,40 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EvernoteCloneGUI.ViewModels
 {
     /// <summary>
     /// The root viewmodel, this is the viewmodel which handles the main application screen.
     /// </summary>
-    public class NoteFeverViewModel : Screen
+    public class NoteFeverViewModel : Conductor<object>
     {
-        
+        protected override void OnActivate()
+        {
+            // Only do this when a note has been opened, otherwise the right side should still be empty.
+            ActivateItem(new NewNoteViewModel());
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void NewNote()
+        {
+            IWindowManager windowManager = new WindowManager();
+            
+            dynamic settings = new ExpandoObject();
+            settings.Height = 600;
+            settings.Width = 800;
+            settings.SizeToContent = SizeToContent.Manual;
+
+            NewNoteViewModel newNoteViewModel = new NewNoteViewModel();
+            windowManager.ShowDialog(newNoteViewModel, null, settings);
+        }
+
     }
 }
