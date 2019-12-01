9111d2605a6e3a7bdde19f3098234cc37ac4e4da
Lucas Ouwens <lucasouwens@gmail.com>
2019-12-01 16:32:16 +0100

    Added documentation to existing ViewModels

--- EvernoteClone/EvernoteCloneGUI/ViewModels/NoteFeverViewModel.cs
+++ EvernoteClone/EvernoteCloneGUI/ViewModels/NoteFeverViewModel.cs
@@ -32,7 +32,7 @@ namespace EvernoteCloneGUI.ViewModels
         public Notebook SelectedNotebook;
         public Note SelectedNote;
 
-        private int UserID = 3; // TODO change this!!!
+        private int UserID = -1; // TODO change this!!!
 
         public NotebookViewModel NotebookViewModel { get; set; }
         public ObservableCollection<TreeViewItem> NotebooksTreeView { get; } = new ObservableCollection<TreeViewItem>(new List<TreeViewItem>());
