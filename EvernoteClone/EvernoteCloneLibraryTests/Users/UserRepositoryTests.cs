12babcf27233fef5053638f6bad3f58997ab20ea
Chino <ShirohigeKaizoku@protonmail.com>
2019-12-02 14:03:24 +0100

    User Unit testing

---
+++ EvernoteClone/EvernoteCloneLibraryTests/Users/UserRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EvernoteCloneLibrary.Users;
+using NUnit.Framework;
+using System.Linq;
+
+
+namespace EvernoteCloneLibraryTests.Users
+{
+    [TestFixture]
+    public class UserRepositoryTest
+    {
+        
+        [TestCase("yrs@hotmail.com", "Appeltje123!", "Nice", "GoodHustle")]
+        
+        public void insertNewUser(string Username, string Password, string FirstName, string LastName)
+        {
+            
+            //Arrange
+
+            UserRepository userRepository = new UserRepository();
+            UserModel users = new UserModel()
+            {
+                Username = Username,
+                Password = Password,
+                FirstName = FirstName,
+                LastName = LastName,
+                CreationDate = DateTime.Now.Date
+
+            };
+
+            //Act            
+             var result = userRepository.Insert(users);
+
+            //Assert
+
+            Assert.IsTrue(result);
+
+        }
+
+        [TestCase]
+
+        public void 
+    }
+
+}
