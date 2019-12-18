21f3e0f4a08a14ed37749c1d150749cf2ff1c29e
Joris Vos <jorisvos2000@gmail.com>
2019-12-18 15:43:00 +0100

    Made a beginning with the LanguageView

--- EvernoteClone/EvernoteCloneGUI/ViewModels/Settings/LanguageViewModel.cs
+++ EvernoteClone/EvernoteCloneGUI/ViewModels/Settings/LanguageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Caliburn.Micro;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,6 @@ namespace EvernoteCloneGUI.ViewModels.Settings
 {
     public class LanguageViewModel : Screen
     {
+        public ObservableCollection<Language> Languages;
     }
 }
